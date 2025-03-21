69cd4c88f94fa037df6d23d016f0f566f3b47d2f
Esad Toplica <esadtoplica1995@gmail.com>
2025-03-21 21:59:05 +0100

    Complete DevOps lab with GitHub Actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,24 @@ provider "azurerm" {
   tenant_id       = var.azure_tenant_id
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+  numeric = true
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "home-lab-rg"
   location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "devopsacr${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -32,8 +47,11 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
-}
 
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
 resource "azurerm_key_vault" "devops_vault" {
   name                = "devops-keyvault-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
@@ -66,14 +84,6 @@ resource "azurerm_key_vault_access_policy" "devops_automation_policy" {
   secret_permissions = ["Get", "List", "Set", "Delete"]
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "devopsacr${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-
 resource "azurerm_key_vault_secret" "acr_username" {
   name         = "acr-username"
   value        = azurerm_container_registry.acr.admin_username
@@ -96,48 +106,6 @@ resource "azurerm_log_analytics_workspace" "log_analytics" {
   retention_in_days   = 30
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-  numeric = true
-}
-
-resource "azurerm_container_group" "devops_api" {
-  name                = "devops-api-instance"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-
-  depends_on = [
-    azurerm_container_registry.acr,
-    azurerm_key_vault_secret.acr_username,
-    azurerm_key_vault_secret.acr_password
-  ]
-
-  container {
-    name   = "devops-api"
-    image  = "${azurerm_container_registry.acr.login_server}/devops-api:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 3000
-      protocol = "TCP"
-    }
-
-    environment_variables = {
-      NODE_ENV = "production"
-    }
-  }
-
-  image_registry_credential {
-    server   = azurerm_container_registry.acr.login_server
-    username = azurerm_key_vault_secret.acr_username.value
-    password = azurerm_key_vault_secret.acr_password.value
-  }
-}
-
 resource "azurerm_monitor_diagnostic_setting" "aks_monitoring" {
   name                       = "aks-monitoring"
   target_resource_id         = azurerm_kubernetes_cluster.aks.id
@@ -147,4 +115,9 @@ resource "azurerm_monitor_diagnostic_setting" "aks_monitoring" {
     category = "AllMetrics"
     enabled  = true
   }
+}
+
+resource "kubernetes_manifest" "ingress_nginx" {
+  manifest   = yamldecode(file("${path.module}/k8s/nginx-ingress-controller.yaml"))
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
